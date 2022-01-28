3a881e55897bf20d4336184a95d8686db13933ac
Harsh Aswar <79035265+harshaswar@users.noreply.github.com>
2022-01-28 23:21:29 +0530

    Model created and registered

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'StudentMS'
+
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,15 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'SMS',
+         'USER': 'root',
+         'PASSWORD': '',
+         'HOST': '127.0.0.1',
+         'PORT': '3307'
+     }
+
 }
 
 
--- SMS/StudentMS/models.py
+++ SMS/StudentMS/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from random import choices
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    roll = models.IntegerField(default=0)
+    regno = models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
+    div = models.IntegerField(default =0,choices=[(1,1),(2,2),(3,3),(4,4),(5,5)])
+    profile = models.ImageField(default=None, null=True)
+
+    def __str__(self):
+        return str(self.regno)
+
+class Leaves(models.Model):
+    regno = models.ForeignKey(Student,on_delete=models.CASCADE)
+    roll = models.IntegerField(default=0)
+    name = models.CharField(max_length=200)
+    div = models.IntegerField(default =0,choices=[(1,1),(2,2),(3,3),(4,4),(5,5)])
