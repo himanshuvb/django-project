cc60cd9869812daa7af6ed723b755c6c73a153ec
Harsh Aswar <79035265+harshaswar@users.noreply.github.com>
2022-01-30 10:43:58 +0530

    Added backup database

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -83,7 +83,11 @@ DATABASES = {
          'PASSWORD': '',
          'HOST': '127.0.0.1',
          'PORT': '3307'
-     }
+     }, 
+     'BACKUP':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 }
 
